7e4ec73d9aae4aa0a215bb8dcaf1ee64f9d49b68
Niraj.Bhivasane <Niraj.Bhivasane@almullaexchange.com>
2024-08-04 23:20:01 +0530

    changes done

--- src/modules/home/CarouselComponent.jsx
+++ src/modules/home/CarouselComponent.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
-import "./crouselComponent.css"
+import "./crouselComponent.css"; // Make sure the CSS file name matches exactly
 
+import Jewel1 from "../../util/images/jewel1.png";
+import Jewel2 from "../../util/images/jewel2.png";
+import Jewel3 from "../../util/images/jewel3.png";
+import Jewel4 from "../../util/images/jewel4.png";
+import Jewel5 from "../../util/images/jewel5.png";
+import Jewel6 from "../../util/images/jewel6.png";
 
 function CarouselComponent() {
   const settings = {
@@ -12,6 +18,8 @@ function CarouselComponent() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, // Adjust the speed as needed
     responsive: [
       {
         breakpoint: 1024,
@@ -29,30 +37,54 @@ function CarouselComponent() {
         },
       },
     ],
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
   };
 
   return (
     <Slider {...settings}>
       <div>
-        <img src="https://via.placeholder.com/300x200" alt="Image 1" />
+        <img src={Jewel1} alt="Image 1" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/300x200" alt="Image 2" />
+        <img src={Jewel2} alt="Image 2" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/300x200" alt="Image 3" />
+        <img src={Jewel3} alt="Image 3" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/300x200" alt="Image 4" />
+        <img src={Jewel4} alt="Image 4" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/300x200" alt="Image 5" />
+        <img src={Jewel5} alt="Image 5" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/300x200" alt="Image 6" />
+        <img src={Jewel6} alt="Image 6" />
       </div>
     </Slider>
   );
 };
 
-export default CarouselComponent
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "rgba(255, 255, 255, 0.7)", borderRadius: "50%", right: "10px" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "rgba(255, 255, 255, 0.7)", borderRadius: "50%", left: "10px", zIndex: 1 }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default CarouselComponent;
