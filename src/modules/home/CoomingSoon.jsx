25ebb05d6077df2853b90143ca84cd69f99e3311
Niraj.Bhivasane <Niraj.Bhivasane@almullaexchange.com>
2024-08-07 19:31:13 +0530

    changes done

---
+++ src/modules/home/CoomingSoon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CoomingSoon() {
+  return (
+    <div style={{display:"flex", justifyContent:"center",alignItems:"center", marginTop:"300px", fontSize:"30px"}}>Coming Soon</div>
+  )
+}
+
+export default CoomingSoon
